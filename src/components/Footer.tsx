b451965f7b7e8820d539407e84b598ff57788282
wilfredo fermin <wfermin@viva.com.do>
2024-02-04 19:01:41 -0400

    restablecer preguntas

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,22 @@
-import { Chip, Stack } from "@mui/material";
+import { Button, Chip, Stack } from "@mui/material";
 import { userQuestionData } from "../hooks/userQuestionData";
+import { useQuetionsState } from "../stores/question";
 
 export const Footer = () => {
   const { correct, incorrect, unanswered } = userQuestionData();
+ const question  = useQuetionsState(state => state.reset)
 
   return (
+    <>
+    
     <Stack direction="row" spacing={1} justifyContent={"center"} marginTop={2}>
       <Chip label={correct} color="success" variant="outlined" />
       <Chip label={incorrect} color="error" variant="outlined" />
       <Chip label={unanswered} variant="outlined" />
     </Stack>
+
+    <Button  onClick={()=> question()} variant="contained" sx={{mt:6}}>Restablecer</Button>
+    
+    </>
   );
 };
--- src/stores/question.tsx
+++ src/stores/question.tsx
@@ -1,80 +1,96 @@
 import { create } from "zustand";
 import { type Question } from "../types";
+import { persist } from "zustand/middleware";
 import confetti from "canvas-confetti";
 
 interface State {
   questions: Question[]; //----> Aqui recibire las pregutas tipadas
   currentQuestion: number; //----> Pregunta seleccionada
   fechQuestion: (limit: number) => Promise<void>; //----> Funcion para obtener las preguntas y actualizar
+  reset: () => void;
 
   // Navegacion
-  goNextQuestion: ()=> void;
-  goPreviusQuestion: () => void
-
+  goNextQuestion: () => void;
+  goPreviusQuestion: () => void;
 
   // Esta funcion correponde al evento al momento de hacer click en la respuesta
   selectAnswer: (questionId: number, answerIndex: number) => void;
 }
 
-export const useQuetionsState = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0, //----> Inicializamos la posicion del array de la pregunta(question)
+export const useQuetionsState = create<State>()(
+  persist(
+    (set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0, //----> Inicializamos la posicion del array de la pregunta(question)
 
-    fechQuestion: async (limit: number) => {
-      // Consultamos y obtenemos nuestros datos
-      const res = await fetch("http://localhost:5173/data.json");
-      const json = await res.json();
+        fechQuestion: async (limit: number) => {
+          // Consultamos y obtenemos nuestros datos
+          const res = await fetch("http://localhost:5173/data.json");
+          const json = await res.json();
 
-      // Obtenemos los datos de manero aleatoria
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
+          // Obtenemos los datos de manero aleatoria
+          const questions = json
+            .sort(() => Math.random() - 0.5)
+            .slice(0, limit);
 
-      // Aqui actualizo el estado
-      set({ questions });
-    },
+          // Aqui actualizo el estado
+          set({ questions });
+        },
 
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      //Mediante get obtemos acceso al estado. Lo destructuramos para solo obtener el datos que nos intersesa,en esta caso es question
-      const { questions } = get();
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          //Mediante get obtemos acceso al estado. Lo destructuramos para solo obtener el datos que nos intersesa,en esta caso es question
+          const { questions } = get();
 
-      //Aqui estamos usando structuredClone para clonar el objeto. En otras palabras, copiaremos todas las preguntas y cambiaremos aquellas que necesitemos cambiar.
-      const newQuestions = structuredClone(questions);
+          //Aqui estamos usando structuredClone para clonar el objeto. En otras palabras, copiaremos todas las preguntas y cambiaremos aquellas que necesitemos cambiar.
+          const newQuestions = structuredClone(questions);
 
-      //Buscaremos el indice de la pregunta que ya tenemos clonado en base al valor recibido como parametro <questionId>
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+          //Buscaremos el indice de la pregunta que ya tenemos clonado en base al valor recibido como parametro <questionId>
+          const questionIndex = newQuestions.findIndex(
+            (q) => q.id === questionId
+          );
 
-      //Una vez tememos el indice,ya podemos buscar la informacion
-      const questionInfo = newQuestions[questionIndex];
+          //Una vez tememos el indice,ya podemos buscar la informacion
+          const questionInfo = newQuestions[questionIndex];
 
-      //Ahora ya con los datos(questionInfo) validamos si el usuario ha hecho bien la respuesta. Este valos nos llega como parametro(answerIndex)
-      const isCorrectoSelectAnswer = questionInfo.correctAnswer === answerIndex;
+          //Ahora ya con los datos(questionInfo) validamos si el usuario ha hecho bien la respuesta. Este valos nos llega como parametro(answerIndex)
+          const isCorrectoSelectAnswer =
+            questionInfo.correctAnswer === answerIndex;
 
-      if (isCorrectoSelectAnswer) confetti();
+          if (isCorrectoSelectAnswer) confetti();
 
-      //Modificamos los datos mutados para posteor actualizar
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectoSelectAnswer,
-        userSelectedAnswer: answerIndex,
+          //Modificamos los datos mutados para posteor actualizar
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectoSelectAnswer,
+            userSelectedAnswer: answerIndex,
+          };
+
+          //Finalmente, actualizamos los datos con la mutacion previamente realizada.
+          set({ questions: newQuestions });
+        },
+
+        reset:()=> set({  questions: [], currentQuestion: 0}),
+
+        //----> Navegacion
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get();
+          const NexQuestion = currentQuestion + 1;
+
+          if (NexQuestion < questions.length)
+            set({ currentQuestion: NexQuestion });
+        },
+
+        goPreviusQuestion: () => {
+          const { currentQuestion } = get();
+          const previusQuestion = currentQuestion - 1;
+
+          if (previusQuestion >= 0) set({ currentQuestion: previusQuestion });
+        },
       };
-
-      //Finalmente, actualizamos los datos con la mutacion previamente realizada.
-      set({ questions: newQuestions });
     },
-
-    //----> Navegacion 
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const NexQuestion = currentQuestion + 1;
-
-      if (NexQuestion < questions.length) set({ currentQuestion: NexQuestion });
-    },
-
-    goPreviusQuestion: () => {
-      const { currentQuestion } = get();
-      const previusQuestion = currentQuestion - 1;
-
-      if (previusQuestion >= 0) set({ currentQuestion: previusQuestion });
-    },
-  };
-});
+    {
+      name: "questions",
+    }
+  )
+);
